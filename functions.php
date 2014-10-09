8b3f140d4daf42718e3bc32bd635ee79f9c9949b
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2014-10-09 09:02:38 +0200

    Modify functions.php

--- functions.php
+++ functions.php
@@ -159,6 +159,7 @@ function assignement_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+            
 }
 add_action( 'wp_enqueue_scripts', 'assignement_scripts' );
 
